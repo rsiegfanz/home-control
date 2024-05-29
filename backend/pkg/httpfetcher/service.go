fa1c061dd483f596e0315f42c5a370f12f1d2764
rs <robertsiggi@online.de>
2024-05-29 16:01:15 +0200

    fix

--- backend/pkg/httpfetcher/service.go
+++ backend/pkg/httpfetcher/service.go
@@ -2,17 +2,20 @@ package httpfetcher
 
 import (
 	"io"
+	"log"
 	"net/http"
 )
 
 func NewService() {
 }
 
-func fetch() {
-	resp, err := http.Get("http://example.com/")
+func fetch(url string, savePath: string) {
+	resp, err := http.Get(url)
 	if err != nil {
-		// handle error
+		log.Println("http error: ", err))
+    return
 	}
+
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 }
